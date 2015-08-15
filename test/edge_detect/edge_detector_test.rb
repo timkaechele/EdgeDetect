5b7c6f8be07d99b743d2fac9283e3561181e6199
Tim Kächele <mail@timkaechele.me>
2015-08-15 09:47:08 +0200

    added a test shoe picture

--- test/edge_detect/edge_detector_test.rb
+++ test/edge_detect/edge_detector_test.rb
@@ -31,4 +31,17 @@ class EdgeDetectorTest < Minitest::Test
     sobel.detect_edges.save(output_sobel)
   end
 
+  def test_robot
+    input = File.expand_path('../../pictures/asics.png', __FILE__)
+    image = ChunkyPNG::Image.from_file(input)
+
+    standard = EdgeDetector.new(image)
+    sobel  = SobelEdgeDetector.new(image)
+
+    output_standard = File.expand_path('../../output/asics_standard.png', __FILE__)
+    output_sobel = File.expand_path('../../output/asics_sobel.png', __FILE__)
+
+    standard.detect_edges.save(output_standard)
+    sobel.detect_edges.save(output_sobel)
+  end
 end
